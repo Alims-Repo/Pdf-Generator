47077a29c3161418a2960f738846d67d2d0332bf
Alim <sourav.0.alim@gmail.com>
2026-01-16 12:38:35 +0600

    Watermark position added in separate file

--- pdf-generator/src/main/java/io/github/alimsrepo/pdf/generator/config/Watermark.kt
+++ pdf-generator/src/main/java/io/github/alimsrepo/pdf/generator/config/Watermark.kt
@@ -73,19 +73,3 @@ sealed class Watermark {
         )
     }
 }
-
-/**
- * Position options for watermarks
- */
-enum class WatermarkPosition {
-    TOP_LEFT,
-    TOP_CENTER,
-    TOP_RIGHT,
-    CENTER_LEFT,
-    CENTER,
-    CENTER_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_CENTER,
-    BOTTOM_RIGHT
-}
-
---
+++ pdf-generator/src/main/java/io/github/alimsrepo/pdf/generator/config/WatermarkPosition.kt
@@ -0,0 +1,16 @@
+package io.github.alimsrepo.pdf.generator.config
+
+/**
+ * Position options for watermarks
+ */
+enum class WatermarkPosition {
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    CENTER_LEFT,
+    CENTER,
+    CENTER_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_CENTER,
+    BOTTOM_RIGHT
+}
